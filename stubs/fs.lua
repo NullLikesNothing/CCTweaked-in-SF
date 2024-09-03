a111ec709d19579b40edb3a2191a14133a47fa95
void pointer <82908498+NullLikesNothing@users.noreply.github.com>
2024-09-02 22:41:02 -0500

    Add more functions to fs

--- stubs/fs.lua
+++ stubs/fs.lua
@@ -17,6 +17,7 @@ local t_concat = table.concat
 local ss = bit.stringstream
 local s_bw = string.startWith
 local spt = string.split
+local unpack = unpack
 
 local function CLOSED_FILE()
     error( "attempt to use a closed file" )
@@ -25,6 +26,19 @@ end
 local sub = string.sub
 local snp = string.normalizePath
 local fs = {}
+local _internal_fs
+local pack = table.pack
+local function traverse( pth, root )
+    local last = root or fsys
+    for i = 1, #pth do
+        if not ( last and last.f and last.f[pth[i]] ) then
+            return
+        end
+        last = last.f[pth[i]]
+    end
+    return last
+end
+
 function fs.open( file, mode )
     expect( 1, file, "string" )
     file = snp( file )
@@ -35,16 +49,14 @@ function fs.open( file, mode )
     if s_bw( file, "rom/" ) and mode ~= "r" and mode ~= "rb" then return nil, "/" .. tostring( file ) .. ": Access denied" end
     if mode == "w" or mode == "wb" then
         local ks = spt( file, "/" )
-        local last = fsys
-        for i = 1, #ks - 1 do
-            if not last.f[ks[i]] then
-                return
-            end
-            last = last.f[ks[i]]
-        end
+        local last = traverse( pack( unpack( ks, 1, #ks - 1 ) ) )
 
         last.f[ks[#ks]] = last.f[ks[#ks]] or { d = "", t = "f" }
-        last = last.f[ks[#ks]]
+        local fl = last.f[ks[#ks]]
+        last.f[ks[#ks]] = fl or { t = "f", d = "" }
+        last = fl
+        if last.t ~= "f" then return nil, "/" .. tostring( file ) .. ": Not a file" end
+
         local f = ss( "" )
         local t = {}
         function t.close()
@@ -85,16 +97,15 @@ function fs.open( file, mode )
     end
     if mode == "a" or mode == "ab" then
         local ks = spt( file, "/" )
-        local last = fsys
-        for i = 1, #ks - 1 do
-            if not last.f[ks[i]] then
-                return
-            end
-            last = last.f[ks[i]]
-        end
+        local last = traverse( pack( unpack( ks, 1, #ks - 1 ) ) )
 
         last.f[ks[#ks]] = last.f[ks[#ks]] or { d = "", t = "f" }
         last = last.f[ks[#ks]]
+        local fl = last.f[ks[#ks]]
+        last.f[ks[#ks]] = fl or { t = "f", d = "" }
+        last = fl
+        if last.t ~= "f" then return nil, "/" .. tostring( file ) .. ": Not a file" end
+
         local f = ss( last.d or "" )
         f:seek( f:size() + 1 )
         local t = {}
@@ -122,13 +133,7 @@ function fs.open( file, mode )
     end
     if mode == "r" or mode == "rb" then
         local ks = spt( file, "/" )
-        local last = fsys
-        for i = 1, #ks do
-            if not last.f[ks[i]] then
-                return nil, "/" .. tostring( file ) .. ": No such file"
-            end
-            last = last.f[ks[i]]
-        end
+        local last = traverse( ks )
         if not last then return nil, "/" .. tostring( file ) .. ": No such file" end
         if last.t ~= "f" then return nil, "/" .. tostring( file ) .. ": Not a file" end
 
@@ -173,7 +178,7 @@ function fs.open( file, mode )
         return t
     end
 
-    return nil, "/" .. tostring( file ) .. ": No such file"
+    return nil, "/" .. file .. ": No such file"
 end
 function fs.combine( ... )
     for i = 1, select( "#", ... ) do
@@ -188,26 +193,20 @@ function fs.exists( pth )
     expect( 1, pth, "string" )
     pth = snp( pth )
     local ks = spt( pth, "/" )
-    local last = fsys
-    for i = 1, #ks do
-        if not last.f[ks[i]] then
-            return false
-        end
-        last = last.f[ks[i]]
-    end
+    local last = traverse( unpack( ks, 1, #ks ) )
     return last ~= nil
+end
+function fs.isReadOnly( pth )
+    expect( 1, pth, "string" )
+    pth = snp( pth )
+    return s_bw( pth, "rom/" )
 end
 function fs.isDir( pth )
     expect( 1, pth, "string" )
     pth = snp( pth )
     local ks = spt( pth, "/" )
-    local last = fsys
-    for i = 1, #ks do
-        if not last.f[ks[i]] then
-            return false
-        end
-        last = last.f[ks[i]]
-    end
+    if pth == "" then return true end
+    local last = traverse( ks ) or {}
     return last.t == "d"
 end
 function fs.list( pth )
@@ -215,12 +214,10 @@ function fs.list( pth )
     pth = snp( pth )
     local ks = spt( pth, "/" )
     local last = fsys
-    for i = 1, #ks do
-        if not last.f[ks[i]] then
-            return nil
-        end
-        last = last.f[ks[i]]
+    if pth ~= "" then
+        last = traverse( ks ) or {}
     end
+    if last.t ~= "d" then error( "/" .. pth .. ": Not a directory" ) end
     local t = {}
     for k, v in pairs( last.f ) do
         t[#t + 1] = k
@@ -228,5 +225,56 @@ function fs.list( pth )
 
     return t
 end
+function fs.makeDir( pth )
+    expect( 1, pth, "string" )
+    pth = snp( pth )
+    local ks = spt( pth, "/" )
+    if ks[1] == "rom" then error( "/" .. pth .. ": Access denied" ) end
 
+    local last = fsys
+    if pth ~= "" then
+        for i = 1, #ks - 1 do
+            if not last.f[ks[i]] then
+                return
+            end
+            last = last.f[ks[i]]
+        end
+        local d = last.f[ks[#ks]]
+        if d then
+            if d.t ~= "d" then error( "/" .. pth .. ": File exists" ) end
+            return
+        end
+        last.f[ks[#ks]] = mkdir()
+    end
+end
+function fs.getFreeSpace()
+    return "unlimited"
+end
+function fs.getCapacity( pth )
+    expect( 1, pth, "string" )
+    pth = snp( pth )
+    local ks = spt( pth, "/" )
+    if ks[1] == "rom" then return end
+
+    return 2 ^ 22
+end
+function fs.attributes( pth )
+    expect( 1, pth, "string" )
+    pth = snp( pth )
+    local ks = spt( pth, "/" )
+
+    return {
+        created = 0,
+        isDir = _internal_fs.isDir( pth ),
+        isReadOnly = ks[1] == "rom",
+        modification = 0,
+        modified = 0,
+        size = 0
+    }
+end
+function fs.getDrive( pth )
+
+end
+
+_internal_fs = table.copy( fs )
 return fs
